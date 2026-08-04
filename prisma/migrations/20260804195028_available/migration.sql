d5a35e616c5bd88656f84014ba109fb35e97adb6
Yody Reta <juditreta@gmail.com>
2026-08-04 15:56:09 -0600

    first commit

---
+++ prisma/migrations/20260804195028_available/migration.sql
@@ -0,0 +1,17 @@
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_product" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "price" REAL NOT NULL,
+    "available" BOOLEAN NOT NULL DEFAULT true,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+INSERT INTO "new_product" ("createdAt", "id", "name", "price", "updatedAt") SELECT "createdAt", "id", "name", "price", "updatedAt" FROM "product";
+DROP TABLE "product";
+ALTER TABLE "new_product" RENAME TO "product";
+CREATE UNIQUE INDEX "product_name_key" ON "product"("name");
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
---
+++ src/products/products.service.ts
@@ -0,0 +1,78 @@
+import { Injectable, NotFoundException, OnModuleInit } from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { PrismaService } from 'src/prisma.service';
+import { PaginationDto } from 'src/common/dto/pagination.dto';
+
+@Injectable()
+export class ProductsService extends PrismaService implements OnModuleInit {
+
+  onModuleInit() {
+    this.$connect();
+  }
+
+  async create(createProductDto: CreateProductDto) {
+    return this.product.create({
+      data: createProductDto
+    });
+  }
+
+  async findAll( paginationDto: PaginationDto ) {
+
+    const { page, limit } = paginationDto;
+
+    const totalPages = await this.product.count({ where: { available: true } });
+    const lastPage = Math.ceil( totalPages / limit );
+
+    return {
+      data: await this.product.findMany({
+        skip: ( page - 1 ) * limit,
+        take: limit,
+        where: {
+          available: true
+        }
+      }),
+      meta: {
+        total: totalPages,
+        page: page,
+        lastPage: lastPage,
+      }
+    }
+  }
+
+  async findOne(id: number) {
+    const product = await this.product.findFirst({
+      where: {
+        id, available: true
+      }
+    });
+    if (!product) {
+      throw new NotFoundException(`Product with ID ${id} not found`);
+    }
+    return product;
+  }
+
+  async update(id: number, updateProductDto: UpdateProductDto) {
+    const {id: __, ...data} = updateProductDto;
+    await this.findOne(id);
+
+    return this.product.update({
+      where: { id },
+      data: data
+    });
+  }
+
+  async remove(id: number) {
+    await this.findOne(id);
+
+    // return this.product.delete({
+    //   where: { id }
+    // });
+    const product = await this.product.update({
+      where: { id },
+      data: { available: false }
+    });
+
+    return product;
+  }
+}
